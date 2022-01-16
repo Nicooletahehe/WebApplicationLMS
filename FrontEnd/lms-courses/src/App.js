31002149e5cedbe2b122adb631b84453d3c05a9a
nicoleta <nicoleta.ungureanu@partner.bmwgroup.com>
2022-01-16 19:11:37 +0200

    FE (begining)

---
+++ FrontEnd/lms-courses/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 class="d-flex justify-content-center m-3"></h3>
+    </div>
+  );
+}
+
+export default App;
